05aa62bd895efee5ddcceb166615cf9a0b9d8ac7
andre-morales <an.dre.m@outlook.com>
2022-02-07 12:44:20 -0300

    GNU Make is now used for building xtldr_core more efficiently. Makefiles might be used for other building processes in the future.

--- XtOS/Bootloader/build.bat
+++ XtOS/Bootloader/build.bat
@@ -4,16 +4,17 @@ setlocal EnableExtensions EnableDelayedExpansion
 set RC=call scripts\rc -sh C:\cygwin64\bin\bash.exe
 set CASM=call scripts\casm
 set VHD="..\..\Test Machines\vdisk.vhd"
+set part=0x108000
 
 if "%1"=="boot_head" (
 	if "%2"=="lba" (
-		!CASM! -If ../../Libs/casm -+d LBA_AVAILABLE -i src/boot_head.asm -tti build/boot_head_lba.asm -ati build/bin/boot_head_lba.img -off 0x3E -off 0x10803E -len 450 -wt !VHD!
+		!CASM! -If ../../Libs/casm -+d LBA_AVAILABLE -i src/boot_head.asm -tti build/boot_head_lba.asm -ati build/bin/boot_head_lba.img -off 0x3E -off !part!+0x3E -len 450 -wt !VHD!
 	) else (
-		!CASM! -If ../../Libs/casm --d LBA_AVAILABLE -i src/boot_head.asm -tti build/boot_head_chs.asm -ati build/bin/boot_head_chs.img -off 0x3E -off 0x10803E -len 450 -wt !VHD!
+		!CASM! -If ../../Libs/casm --d LBA_AVAILABLE -i src/boot_head.asm -tti build/boot_head_chs.asm -ati build/bin/boot_head_chs.img -off 0x3E -off !part!+0x3E -len 450 -wt !VHD!
 	)
 )
 if "%1"=="boot_core" (
-	!CASM! -If ../../Libs/casm -i src/boot_core.asm -tti build/boot_core.asm -ati build/bin/boot_core.img -off 0x0 -off 0x108200 -len 2560 -wt !VHD!
+	!CASM! -If ../../Libs/casm -i src/boot_core.asm -tti build/boot_core.asm -ati build/bin/boot_core.img -off 0x0 -off !part!+0x200 -len 2560 -wt !VHD!
 )
 if "%1"=="ldr_head" (
 	!CASM! -If ../../Libs/casm -i src/xtloader_head.asm -tti build/xtloader_head.asm -ati build/bin/LDRHEAD.BIN
@@ -30,22 +31,7 @@ if "%1"=="ldr_head" (
 	)
 )
 if "%1"=="ldr_core" (
-	set CFLAGS=-ffreestanding -std=gnu99 -nostdlib -lgcc -Qn -O1
-	echo Building using '!CFLAGS!'
-	
-	!RC! --nest_cmds -sc ldr_core_scr.sh
-	
-	goto :eof
-	echo Moving...
-	diskpart /s scripts\mountdisk.dps
-	copy "bin\XTLOADER.ELF" "Z:\XTOS\XTLOADER.ELF"
-	echo Done.
-)
-if "%1"=="mnt" (
-	diskpart /s scripts\mountdisk.dps
-)
-if "%1"=="umnt" (
-	diskpart /s scripts\unmountdisk.dps
+	make xtldr_core
 )
 goto :eof
 
--- XtOS/Bootloader/ldr_core_scr.sh
+++
@@ -1,6 +0,0 @@
-@..\..\Tools\GCC\bin\i686-elf-gcc.exe -c @src\ldr_core\core.c -o @build\ldr_core\core.o ${CFLAGS}
-echo Built core.
-@..\..\Tools\GCC\bin\i686-elf-as.exe @src\ldr_core\stub.s -o @build\ldr_core\stub.o
-echo Built stub.
-@..\..\Tools\GCC\bin\i686-elf-gcc.exe -T @src\ldr_core\link.ld -o @build\bin\XTLOADER.ELF @build\ldr_core\stub.o @build\ldr_core\core.o ${CFLAGS}
-echo Linked.
